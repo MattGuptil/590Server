695b47acc837eafb5cc9b091bc08848c8b6ee4ad
Matt Guptil <mdguptil@gmail.com>
2018-11-16 22:28:00 -0500

    Added Docstrings for all functions, including flask handler functions.

--- server.py
+++ server.py
@@ -8,6 +8,12 @@ myUsers = [] ;
 
 @app.route("/heart_rate/average/<name>", methods = ["GET"])
 def get_avgHR(name):
+	""" This GET function grabs the AvgHR for a user.
+
+	Returns:
+		A dictionary of the AvgHR in JSON dictionary form.
+
+	"""
 	myName = "{}".format(name)
 	myResults = dataRetreiver(myName, "AvgHR")
 
@@ -15,14 +21,28 @@ def get_avgHR(name):
 
 @app.route("heart_rate/<name>", methods = ["GET"])
 def get_heartrate(name):
+	""" This GET function grabs the heart rate data of the user entered.
+
+	Returns:
+		Heart rate data of user in JSON dictionary form.
+
+
+	"""
 	myName = "{}".format(name)
 	myResults = dataRetreiver(myName, "HR")
 
 	return myResults
 
 
-@app.route("/hear_rate", methods=["POST"])
+@app.route("/heart_rate", methods=["POST"])
 def heart_rate():
+	""" This is the POST function that allows a user to enter information.
+
+	Returns:
+		Nothing, simply stors the data given by user.		
+
+
+	"""
 	r = request.get_json()
 	email = r['user_email']
 	HR = r['heart_rate']
@@ -40,6 +60,20 @@ def heart_rate():
 	return
 
 def create_NewUser(myE, myHR, myA, myAvg, myTi):
+	""" This function creates a new User object with associated input values.
+
+	Args:
+		myE: String Email address of user.
+		myHR: float64, heart rate of user.
+		myA: int, age of user.
+		myAvg: int, forced average heart rate.
+		myTi: string, time at point of data entry.
+
+	Returns:
+		A User object with associated input values installed.
+
+
+	"""
 	x = User(myE, myHR, myA, myAvg, myTi)
 	return x
 
@@ -65,6 +99,16 @@ def addto_User(myUse, myHR, myA, myTi):
 
 
 def checkNewU(us_email):
+	""" This function takes the users email and checks to see if they exist in memory.
+
+	Args:
+		us_email: String, of Users email
+
+	Returns:
+		True and the object of given user if found, and False if user was not found.
+
+
+	"""
 	for key in myUsers:
 		if key.email() == us_email:
 			return [True, key]
@@ -72,11 +116,34 @@ def checkNewU(us_email):
 
 
 def calcAv(thisUser):
+	""" This function calls the average property function of the User Object.
+
+	Args:
+		thisUser: User Object of current user that will be modified.
+
+	Returns:
+		The User Object with an updated AvgHR property.
+
+
+	"""
 	thisUser.calc_AvgHR()
 	return thisUser
 
 
 def dataRetreiver(name, prop):
+	""" This function will take in user email and output desired results based on call location.
+
+	Args:
+		name: String, This is the users email address in String form
+		prop: String, This is the property of the User object that is being requested.
+
+	Returns:
+		Jsonified dictionary if user was found, along with proper exit code.
+
+	Raises:
+		ValueError: If the email name entered does not match a current user, will return proper exit code.
+
+	"""
 	myResults = checkNewU(name)
 
 	if not myResults[0]:
